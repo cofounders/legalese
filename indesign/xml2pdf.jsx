a7053ebf76e9a73010ef37a56e8cd49ebe6caabf
Wong Meng Weng <mengwong@pobox.com>
2015-03-03 16:09:05 +0800

    change encoding to unix not mac, oops

--- indesign/xml2pdf.jsx
+++ indesign/xml2pdf.jsx
@@ -1,1 +1,25 @@
-﻿// import an XML file into the Legalese template.// this runs as an Adobe InDesign script.#include "xml2pdf-lib.jsx"main();// -------------------------------------------------- mainfunction main(){  var interactive = true;  var xmlFiles = identifyXmlFiles("recurse",  // recurse | queryUser								  Folder("~/Google Drive/Legalese Root"));  var indtFile = identifyIndtFile("hardcoded", // hardcoded | queryUser								  "~/non-db-src/legalese/build/00 legalese template.indt");  if (interactive && xmlFiles.length == 0) { alert ("nothing to do"); }   if (xmlFiles.length > 0) {	app.scriptPreferences.enableRedraw=true; 	xmls2pdf(xmlFiles, indtFile, interactive);	app.scriptPreferences.enableRedraw=true;  }}
+// import an XML file into the Legalese template.
+// this runs as an Adobe InDesign script.
+
+#include "xml2pdf-lib.jsx"
+
+main();
+// -------------------------------------------------- main
+function main(){
+
+  var interactive = true;
+
+  var xmlFiles = identifyXmlFiles("recurse",  // recurse | queryUser
+								  Folder("~/Google Drive/Legalese Root"));
+  var indtFile = identifyIndtFile("hardcoded", // hardcoded | queryUser
+								  "~/non-db-src/legalese/build/00 legalese template.indt");
+
+  if (interactive && xmlFiles.length == 0) { alert ("nothing to do. Is Google Drive synced?"); } 
+
+  if (xmlFiles.length > 0) {
+	app.scriptPreferences.enableRedraw=true; 
+	xmls2pdf(xmlFiles, indtFile, interactive);
+	app.scriptPreferences.enableRedraw=true;
+  }
+}
+
